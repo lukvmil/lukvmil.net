4abc2fa206c5676f1c45a7f8ec1eb7380cb4d3b4
lukvmil <millerluke1364@gmail.com>
2026-08-24 16:24:51 -0400

    added new links

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ projects = {
                 "type": "link",
                 "title": "Digital Infrastructure Research & Development: KOI Network Protocol x Project Interlay",
                 "url": "https://blog.block.science/koi-network-protocol-project-interlay/"
+            },
+            {
+                "type": "link",
+                "title": "A Preview of the KOI-net Protocol",
+                "url": "https://blog.block.science/a-preview-of-the-koi-net-protocol/"
+            },
+            {
+                "type": "link",
+                "title": "Telescope and KOI in Action: Building Feedback Loops for Organisational Intelligence",
+                "url": "https://blog.block.science/building-feedback-loops-telescope-koi"
             }
         ]
     },
@@ -89,7 +99,14 @@ function clickProject(projId) {
         if (infoItem.type === "text") {
             infoCell.innerHTML = infoItem.content;
         } else if (infoItem.type === "link") {
+            let anchor = document.createElement("a")
+            anchor.href = infoItem.url;
+            anchor.style = "display: block;"
             infoCell.innerHTML = infoItem.title;
+            infoCell.innerHTML += ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link-icon lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg>`
+            anchor.appendChild(infoCell);
+            infoColumn.appendChild(anchor);
+            return
         }
         infoColumn.appendChild(infoCell);
     }
